542791b5fef193a38d0d27df28554c3497d6a6ad
Henrique Miranda <enrque.ns@gmail.com>
2020-04-11 20:40:31 -0300

    :sparkles: feat(gostack-11): challenge 01

--- gostack-11/challenge-01/src/app.js
+++ gostack-11/challenge-01/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,67 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (_, res) => {
+  return res.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return res.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+  const index = repositories.findIndex((r) => r.id === id);
+
+  if (index < 0) {
+    return res.status(400).json({ message: "Repository not found." });
+  }
+
+  const { title, url, techs } = req.body;
+
+  repositories[index].title = title;
+  repositories[index].url = url;
+  repositories[index].techs = techs;
+
+  return res.json(repositories[index]);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+  const index = repositories.findIndex((r) => r.id === id);
+
+  if (index < 0) {
+    return res.status(400).json({ message: "Repository not found." });
+  }
+
+  repositories.splice(index, 1);
+
+  return res.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", (req, res) => {
+  const { id } = req.params;
+  const index = repositories.findIndex((r) => r.id === id);
+
+  if (index < 0) {
+    return res.status(400).json({ message: "Repository not found." });
+  }
+
+  repositories[index].likes += 1;
+
+  res.json(repositories[index]);
 });
 
 module.exports = app;
